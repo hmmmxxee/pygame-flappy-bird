d8b6729b708db163ddaa347676019c287d2b9ea0
hmmmxxee <kizzzzz@icloud.com>
2025-04-23 15:17:30 +0200

    Add project files for Space invaders game

--- main.py
+++ main.py
@@ -1,77 +1,102 @@
 import pygame
 import sys
-from sprites.background import City
-from sprites.background import Road
-from sprites.bird import Bird
-from sprites.pipes import Pipe
-from sprites.game import Score
+from sprites.space import Space
+from sprites.spaceship import Spaceship
+from sprites.alien import Alien, generate_aliens
+from sprites.explosion import Explosion
+from sprites.health import Healthbar
+from sprites.game import Game
+
 
 pygame.init()
  
-# Константы/Constants
-WIDTH = 288
-HEIGHT = 512
+# Константы
+WIDTH = 600
+HEIGHT = 700
 FPS = 60
  
-# Создание окна/Window creating
+# Создание окна
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Flappy Bird")
+pygame.display.set_caption("Space Invaders")
 clock = pygame.time.Clock()
  
 def main():
-    # Спрайты/Sprites
-    city = City()
-    road = Road()
-    player = Bird()
-    pipes = pygame.sprite.Group()
-    pipes.add(Pipe())
-    score = Score()
+    # Спрайты
+    space=Space()
+    player_bullets=pygame.sprite.Group()                     
+    player=Spaceship(player_bullets)
+    players=pygame.sprite.GroupSingle()
+    players.add(player)
+    aliens = pygame.sprite.Group()
+    alien_bullets = pygame.sprite.Group()
+    generate_aliens(aliens, alien_bullets)
+    explosions=pygame.sprite.Group()
+    healthbar=Healthbar(player.health)
+    game=pygame.sprite.GroupSingle()
 
-    game_over = False
-    checkpoint = False
+
     running = True
     while running:
-        # Частота обновления экрана/Screen refresh rate
+        # Частота обновления экрана
         clock.tick(FPS)
     
-        # События/Events
+        # События
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
-        if pipes.sprites()[0].rect1.x < player.rect.x and not checkpoint:
-            score.points += 1
-            checkpoint = True
-            score.sound_score.play()
-        for pipe in pipes:
-            if (pipe.rect1.collidepoint(player.rect.midbottom) or 
-                pipe.rect2.collidepoint(player.rect.midtop)) and not game_over:
-                game_over = True
-                player.sound_die.play()                           
-        if (player.rect.top > road.rect.top or player.rect.top < city.rect.top) and not game_over:
-            game_over=True
-            player.sound_die.play()
-    
-        # Рендеринг/Rendering
-        city.draw(screen)
-        for pipe in pipes:
-            pipe.draw(screen)
-        road.draw(screen)
-        player.draw(screen)
-        score.draw(screen)
+        for alien in aliens.sprites():
+            if pygame.sprite.spritecollide(alien, player_bullets, True, pygame.sprite.collide_mask):
+                explosion=Explosion(alien.rect.center)
+                explosion.sound.play()
+                explosions.add(explosion)
+                alien.kill() 
+        for player in players.sprites(): 
+            if pygame.sprite.spritecollide(player, alien_bullets, True, pygame.sprite.collide_mask):
+                player.health -= 1
+                healthbar.hearts.pop()
+                if player.health < 1:
+                    explosion = Explosion(player.rect.center)
+                    explosion.sound.play()
+                    explosions.add(explosion)
+                    player.kill()
+                else:
+                    player.sound_damage.play()
+        if not aliens.sprites():
+            game.add(Game('Y O U  W I N !'))
+            key=pygame.key.get_pressed()
+            if key[pygame.K_ESCAPE]:
+                main()
+        elif not players.sprites():
+            game.add(Game('G A M E  O V E R'))
+            key=pygame.key.get_pressed()
+            if key[pygame.K_ESCAPE]:
+                main()
         
+        # Рендеринг
+        screen.fill((0, 0, 0))
+        space.draw(screen)
+        players.draw(screen)
+        player_bullets.draw(screen)
+        aliens.draw(screen)
+        explosions.draw(screen)
+        alien_bullets.draw(screen)
+        healthbar.draw(screen)
+        game.draw(screen)
+
+        # Обновление спрайтов
+        players.update()
+        player_bullets.update()
+        explosions.update()
+        aliens.update()
+        alien_bullets.update()
+
     
-        # Обновление спрайтов/Updating sprites
-        if not game_over:
-            road.update()
-            player.update()
-            pipes.update()
-            if len(pipes) < 1:
-                pipes.add(Pipe())
-                checkpoint = False
-    
-        # Обновление экрана/Screen Refresh
+        # Обновление экрана
         pygame.display.update()
+        space.update()
  
 if __name__ == "__main__":
     main()
+
+
--- sprites/game.py
+++ sprites/game.py
@@ -1,18 +1,12 @@
 import pygame
 
-
-
-class Score(pygame.sprite.Sprite):
-    def __init__(self):
+class Game(pygame.sprite.Sprite):
+    def __init__(self, phrase):
         super().__init__()
-        self.points = 0
-        self.font = pygame.font.Font(r'C:\Users\49177\Desktop\python files\pygame-flappy-bird\assets\fonts\gamefont.ttf')
-        self.image = self.font.render(f'{self.points}', True, (255,255,255))
-        self.sound_score = pygame.mixer.Sound(r'C:\Users\49177\Desktop\python files\pygame-flappy-bird\assets\sounds\point.wav')
-        self.rect = self.image.get_rect()
+        self.font=pygame.font.Font(r'C:\Users\49177\Desktop\python files\pygame-space-invaders\assets\fonts\gamefont.ttf')
+        self.image=self.font.render(phrase, (255,255,255),(255,255,255))
+        self.rect=self.image.get_rect()
         surface=pygame.display.get_surface()
-        self.rect.center = surface.get_rect().center
-        self.rect.y = 70
+        self.rect.center=surface.get_rect().center
     def draw(self, surface):
         surface.blit(self.image, self.rect)
-        self.image = self.font.render(f'{self.points}', True, (255,255,255))
